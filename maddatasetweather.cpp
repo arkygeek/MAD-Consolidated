b0330f1e55676481b7bc50767aff68c620afa964
Jason Jorgenson <jjorgenson@gmail.com>
2014-06-19 16:51:46 +0100

    dataset weather class added

--- maddatasetweather.cpp
+++ maddatasetweather.cpp
@@ -25,3 +25,147 @@
 MadDatasetWeather::MadDatasetWeather()
 {
 }
+
+MadDatasetWeather::~MadDatasetWeather()
+{
+}
+
+// accessors
+
+QString MadDatasetWeather::notes() const {return mNotes;}
+
+bool MadDatasetWeather::precipitationMeasured() const{return mPrecipitationMeasured;}
+bool MadDatasetWeather::tAveMeasured() const{return mTAveMeasured;}
+bool MadDatasetWeather::tMinMeasured() const{return mTMinMeasured;}
+bool MadDatasetWeather::tMaxMeasured() const{return mTMaxMeasured;}
+bool MadDatasetWeather::relativeHumidityMeasured() const{return mRelativeHumidityMeasured;}
+bool MadDatasetWeather::windSpeedMeasured() const{return mWindSpeedMeasured;}
+bool MadDatasetWeather::globalRadiationMeasured() const{return mGlobalRadiationMeasured;}
+bool MadDatasetWeather::sunshineHoursMeasured() const{return mSunshineHoursMeasured;}
+bool MadDatasetWeather::leafWetnessMeasured() const{return mLeafWetnessMeasured;}
+bool MadDatasetWeather::soilTempMeasured() const{return mSoilTempMeasured;}
+
+double MadDatasetWeather::precipitationKmToSite() const{return mPrecipitationKmToSite;}
+double MadDatasetWeather::tAveKmToSite() const{return mTAveKmToSite;}
+double MadDatasetWeather::tMinKmToSite() const{return mTMinKmToSite;}
+double MadDatasetWeather::tMaxKmToSite() const{return mTMaxKmToSite;}
+double MadDatasetWeather::relativeHumidityKmToSite() const{return mRelativeHumidityKmToSite;}
+double MadDatasetWeather::windSpeedKmToSite() const{return mWindSpeedKmToSite;}
+double MadDatasetWeather::globalRadiationKmToSite() const{return mGlobalRadiationKmToSite;}
+double MadDatasetWeather::sunshineHoursKmToSite() const{return mSunshineHoursKmToSite;}
+double MadDatasetWeather::leafWetnessKmToSite() const{return mLeafWetnessKmToSite;}
+double MadDatasetWeather::soilTempKmToSite() const{return mSoilTempKmToSite;}
+
+int MadDatasetWeather::precipitationAltitudeDif() const{return mPrecipitationAltitudeDif;}
+int MadDatasetWeather::tAveAltitudeDif() const{return mTAveAltitudeDif;}
+int MadDatasetWeather::tMinAltitudeDif() const{return mTMinAltitudeDif;}
+int MadDatasetWeather::tMaxAltitudeDif() const{return mTMaxAltitudeDif;}
+int MadDatasetWeather::relativeHumidityAltitudeDif() const{return mRelativeHumidityAltitudeDif;}
+int MadDatasetWeather::windSpeedAltitudeDif() const{return mWindSpeedAltitudeDif;}
+int MadDatasetWeather::globalRadiationAltitudeDif() const{return mGlobalRadiationAltitudeDif;}
+int MadDatasetWeather::sunshineHoursAltitudeDif() const{return mSunshineHoursAltitudeDif;}
+int MadDatasetWeather::leafWetnessAltitudeDif() const{return mLeafWetnessAltitudeDif;}
+int MadDatasetWeather::soilTempAltitudeDif() const{return mSoilTempAltitudeDif;}
+
+int MadDatasetWeather::precipitationOptimumDistance() const{return mPrecipitationOptimumDistance;}
+int MadDatasetWeather::tAveOptimumDistance() const{return mTAveOptimumDistance;}
+int MadDatasetWeather::tMinOptimumDistance() const{return mTMinOptimumDistance;}
+int MadDatasetWeather::tMaxOptimumDistance() const{return mTMaxOptimumDistance;}
+int MadDatasetWeather::relativeHumidityOptimumDistance() const{return mRelativeHumidityOptimumDistance;}
+int MadDatasetWeather::windSpeedOptimumDistance() const{return mWindSpeedOptimumDistance;}
+int MadDatasetWeather::globalRadiationOptimumDistance() const{return mGlobalRadiationOptimumDistance;}
+int MadDatasetWeather::sunshineHoursOptimumDistance() const{return mSunshineHoursOptimumDistance;}
+int MadDatasetWeather::leafWetnessOptimumDistance() const{return mLeafWetnessOptimumDistance;}
+int MadDatasetWeather::soilTempOptimumDistance() const{return mSoilTempOptimumDistance;}
+
+double MadDatasetWeather::precipitationWeight() const{return mPrecipitationWeight;}
+double MadDatasetWeather::tAveWeight() const{return mTAveWeight;}
+double MadDatasetWeather::tMinWeight() const{return mTMinWeight;}
+double MadDatasetWeather::tMaxWeight() const{return mTMaxWeight;}
+double MadDatasetWeather::relativeHumidityWeight() const{return mRelativeHumidityWeight;}
+double MadDatasetWeather::windSpeedWeight() const{return mWindSpeedWeight;}
+double MadDatasetWeather::globalRadiationWeight() const{return mGlobalRadiationWeight;}
+double MadDatasetWeather::sunshineHoursWeight() const{return mSunshineHoursWeight;}
+double MadDatasetWeather::leafWetnessWeight() const{return mLeafWetnessWeight;}
+double MadDatasetWeather::soilTempWeight() const{return mSoilTempWeight;}
+
+bool MadDatasetWeather::precipitationState() const{return mPrecipitationState;}
+bool MadDatasetWeather::tAveState() const{return mTAveState;}
+bool MadDatasetWeather::tMinState() const{return mTMinState;}
+bool MadDatasetWeather::tMaxState() const{return mTMaxState;}
+bool MadDatasetWeather::relativeHumidityState() const{return mRelativeHumidityState;}
+bool MadDatasetWeather::windSpeedState() const{return mWindSpeedState;}
+bool MadDatasetWeather::globalRadiationState() const{return mGlobalRadiationState;}
+bool MadDatasetWeather::sunshineHoursState() const{return mSunshineHoursState;}
+bool MadDatasetWeather::leafWetnessState() const{return mLeafWetnessState;}
+bool MadDatasetWeather::soilTempState() const{return mSoilTempState;}
+
+// mutators
+void MadDatasetWeather::setNotes(QString theNotes){mNotes = theNotes;}
+
+void MadDatasetWeather::setPrecipitationMeasured (bool thePrecipitationMeasured){mPrecipitationMeasured = thePrecipitationMeasured;}
+void MadDatasetWeather::setTAveMeasured (bool theTAveMeasured){mTAveMeasured = theTAveMeasured;}
+void MadDatasetWeather::setTMinMeasured (bool theTMinMeasured){mTMinMeasured = theTMinMeasured;}
+void MadDatasetWeather::setTMaxMeasured (bool theTMaxMeasured){mTMaxMeasured = theTMaxMeasured;}
+void MadDatasetWeather::setRelativeHumidityMeasured (bool theRelativeHumidityMeasured){mRelativeHumidityMeasured = theRelativeHumidityMeasured;}
+void MadDatasetWeather::setWindSpeedMeasured (bool theWindSpeedMeasured){mWindSpeedMeasured = theWindSpeedMeasured;}
+void MadDatasetWeather::setGlobalRadiationMeasured (bool theGlobalRadiationMeasured){mGlobalRadiationMeasured = theGlobalRadiationMeasured;}
+void MadDatasetWeather::setSunshineHoursMeasured (bool theSunshineHoursMeasured){mSunshineHoursMeasured = theSunshineHoursMeasured;}
+void MadDatasetWeather::setLeafWetnessMeasured (bool theLeafWetnessMeasured){mLeafWetnessMeasured = theLeafWetnessMeasured;}
+void MadDatasetWeather::setSoilTempMeasured (bool theSoilTempMeasured){mSoilTempMeasured = theSoilTempMeasured;}
+
+void MadDatasetWeather::setPrecipitationKmToSite (double thePrecipitationKmToSite){mPrecipitationKmToSite = thePrecipitationKmToSite;}
+void MadDatasetWeather::setTAveKmToSite (double theTAveKmToSite){mTAveKmToSite = theTAveKmToSite;}
+void MadDatasetWeather::setTMinKmToSite (double theTMinKmToSite){mTMinKmToSite = theTMinKmToSite;}
+void MadDatasetWeather::setTMaxKmToSite (double theTMaxKmToSite){mTMaxKmToSite = theTMaxKmToSite;}
+void MadDatasetWeather::setRelativeHumidityKmToSite (double theRelativeHumidityKmToSite){mRelativeHumidityKmToSite = theRelativeHumidityKmToSite;}
+void MadDatasetWeather::setWindSpeedKmToSite (double theWindSpeedKmToSite){mWindSpeedKmToSite = theWindSpeedKmToSite;}
+void MadDatasetWeather::setGlobalRadiationKmToSite (double theGlobalRadiationKmToSite){mGlobalRadiationKmToSite = theGlobalRadiationKmToSite;}
+void MadDatasetWeather::setSunshineHoursKmToSite (double theSunshineHoursKmToSite){mSunshineHoursKmToSite = theSunshineHoursKmToSite;}
+void MadDatasetWeather::setLeafWetnessKmToSite (double theLeafWetnessKmToSite){mLeafWetnessKmToSite = theLeafWetnessKmToSite;}
+void MadDatasetWeather::setSoilTempKmToSite (double theSoilTempKmToSite){mSoilTempKmToSite = theSoilTempKmToSite;}
+
+void MadDatasetWeather::setPrecipitationAltitudeDif (int thePrecipitationAltitudeDif){mPrecipitationAltitudeDif = thePrecipitationAltitudeDif;}
+void MadDatasetWeather::setTAveAltitudeDif (int theTAveAltitudeDif){mTAveAltitudeDif = theTAveAltitudeDif;}
+void MadDatasetWeather::setTMinAltitudeDif (int theTMinAltitudeDif){mTMinAltitudeDif = theTMinAltitudeDif;}
+void MadDatasetWeather::setTMaxAltitudeDif (int theTMaxAltitudeDif){mTMaxAltitudeDif = theTMaxAltitudeDif;}
+void MadDatasetWeather::setRelativeHumidityAltitudeDif (int theRelativeHumidityAltitudeDif){mRelativeHumidityAltitudeDif = theRelativeHumidityAltitudeDif;}
+void MadDatasetWeather::setWindSpeedAltitudeDif (int theWindSpeedAltitudeDif){mWindSpeedAltitudeDif = theWindSpeedAltitudeDif;}
+void MadDatasetWeather::setGlobalRadiationAltitudeDif (int theGlobalRadiationAltitudeDif){mGlobalRadiationAltitudeDif = theGlobalRadiationAltitudeDif;}
+void MadDatasetWeather::setSunshineHoursAltitudeDif (int theSunshineHoursAltitudeDif){mSunshineHoursAltitudeDif = theSunshineHoursAltitudeDif;}
+void MadDatasetWeather::setLeafWetnessAltitudeDif (int theLeafWetnessAltitudeDif){mLeafWetnessAltitudeDif = theLeafWetnessAltitudeDif;}
+void MadDatasetWeather::setSoilTempAltitudeDif (int theSoilTempAltitudeDif){mSoilTempAltitudeDif = theSoilTempAltitudeDif;}
+
+void MadDatasetWeather::setPrecipitationOptimumDistance (int thePrecipitationOptimumDistance){mPrecipitationOptimumDistance = thePrecipitationOptimumDistance;}
+void MadDatasetWeather::setTAveOptimumDistance (int theTAveOptimumDistance){mTAveOptimumDistance = theTAveOptimumDistance;}
+void MadDatasetWeather::setTMinOptimumDistance (int theTMinOptimumDistance){mTMinOptimumDistance = theTMinOptimumDistance;}
+void MadDatasetWeather::setTMaxOptimumDistance (int theTMaxOptimumDistance){mTMaxOptimumDistance = theTMaxOptimumDistance;}
+void MadDatasetWeather::setRelativeHumidityOptimumDistance (int theRelativeHumidityOptimumDistance){mRelativeHumidityOptimumDistance = theRelativeHumidityOptimumDistance;}
+void MadDatasetWeather::setWindSpeedOptimumDistance (int theWindSpeedOptimumDistance){mWindSpeedOptimumDistance = theWindSpeedOptimumDistance;}
+void MadDatasetWeather::setGlobalRadiationOptimumDistance (int theGlobalRadiationOptimumDistance){mGlobalRadiationOptimumDistance = theGlobalRadiationOptimumDistance;}
+void MadDatasetWeather::setSunshineHoursOptimumDistance (int theSunshineHoursOptimumDistance){mSunshineHoursOptimumDistance = theSunshineHoursOptimumDistance;}
+void MadDatasetWeather::setLeafWetnessOptimumDistance (int theLeafWetnessOptimumDistance){mLeafWetnessOptimumDistance = theLeafWetnessOptimumDistance;}
+void MadDatasetWeather::setSoilTempOptimumDistance (int theSoilTempOptimumDistance){mSoilTempOptimumDistance = theSoilTempOptimumDistance;}
+
+void MadDatasetWeather::setPrecipitationWeight (double thePrecipitationWeight){mPrecipitationWeight = thePrecipitationWeight;}
+void MadDatasetWeather::setTAveWeight (double theTAveWeight){mTAveWeight = theTAveWeight;}
+void MadDatasetWeather::setTMinWeight (double theTMinWeight){mTMinWeight = theTMinWeight;}
+void MadDatasetWeather::setTMaxWeight (double theTMaxWeight){mTMaxWeight = theTMaxWeight;}
+void MadDatasetWeather::setRelativeHumidityWeight (double theRelativeHumidityWeight){mRelativeHumidityWeight = theRelativeHumidityWeight;}
+void MadDatasetWeather::setWindSpeedWeight (double theWindSpeedWeight){mWindSpeedWeight = theWindSpeedWeight;}
+void MadDatasetWeather::setGlobalRadiationWeight (double theGlobalRadiationWeight){mGlobalRadiationWeight = theGlobalRadiationWeight;}
+void MadDatasetWeather::setSunshineHoursWeight (double theSunshineHoursWeight){mSunshineHoursWeight = theSunshineHoursWeight;}
+void MadDatasetWeather::setLeafWetnessWeight (double theLeafWetnessWeight){mLeafWetnessWeight = theLeafWetnessWeight;}
+void MadDatasetWeather::setSoilTempWeight (double theSoilTempWeight){mSoilTempWeight = theSoilTempWeight;}
+
+void MadDatasetWeather::setPrecipitationState (bool thePrecipitationState){mPrecipitationState = thePrecipitationState;}
+void MadDatasetWeather::setTAveState (bool theTAveState){mTAveState = theTAveState;}
+void MadDatasetWeather::setTMinState (bool theTMinState){mTMinState = theTMinState;}
+void MadDatasetWeather::setTMaxState (bool theTMaxState){mTMaxState = theTMaxState;}
+void MadDatasetWeather::setRelativeHumidityState (bool theRelativeHumidityState){mRelativeHumidityState = theRelativeHumidityState;}
+void MadDatasetWeather::setWindSpeedState (bool theWindSpeedState){mWindSpeedState = theWindSpeedState;}
+void MadDatasetWeather::setGlobalRadiationState (bool theGlobalRadiationState){mGlobalRadiationState = theGlobalRadiationState;}
+void MadDatasetWeather::setSunshineHoursState (bool theSunshineHoursState){mSunshineHoursState = theSunshineHoursState;}
+void MadDatasetWeather::setLeafWetnessState (bool theLeafWetnessState){mLeafWetnessState = theLeafWetnessState;}
+void MadDatasetWeather::setSoilTempState (bool theSoilTempState){mSoilTempState = theSoilTempState;}
+
--- maddatasetweather.h
+++ maddatasetweather.h
@@ -39,6 +39,217 @@ public:
   MadDatasetWeather();
   ~MadDatasetWeather();
 
+  // accessors
+
+    QString notes() const;
+
+    bool precipitationMeasured() const;
+    bool tAveMeasured() const;
+    bool tMinMeasured() const;
+    bool tMaxMeasured() const;
+    bool relativeHumidityMeasured() const;
+    bool windSpeedMeasured() const;
+    bool globalRadiationMeasured() const;
+    bool sunshineHoursMeasured() const;
+    bool leafWetnessMeasured() const;
+    bool soilTempMeasured() const;
+
+    double precipitationKmToSite() const;
+    double tAveKmToSite() const;
+    double tMinKmToSite() const;
+    double tMaxKmToSite() const;
+    double relativeHumidityKmToSite() const;
+    double windSpeedKmToSite() const;
+    double globalRadiationKmToSite() const;
+    double sunshineHoursKmToSite() const;
+    double leafWetnessKmToSite() const;
+    double soilTempKmToSite() const;
+
+    int precipitationAltitudeDif() const;
+    int tAveAltitudeDif() const;
+    int tMinAltitudeDif() const;
+    int tMaxAltitudeDif() const;
+    int relativeHumidityAltitudeDif() const;
+    int windSpeedAltitudeDif() const;
+    int globalRadiationAltitudeDif() const;
+    int sunshineHoursAltitudeDif() const;
+    int leafWetnessAltitudeDif() const;
+    int soilTempAltitudeDif() const;
+
+    int precipitationOptimumDistance() const;
+    int tAveOptimumDistance() const;
+    int tMinOptimumDistance() const;
+    int tMaxOptimumDistance() const;
+    int relativeHumidityOptimumDistance() const;
+    int windSpeedOptimumDistance() const;
+    int globalRadiationOptimumDistance() const;
+    int sunshineHoursOptimumDistance() const;
+    int leafWetnessOptimumDistance() const;
+    int soilTempOptimumDistance() const;
+
+    double precipitationWeight() const;
+    double tAveWeight() const;
+    double tMinWeight() const;
+    double tMaxWeight() const;
+    double relativeHumidityWeight() const;
+    double windSpeedWeight() const;
+    double globalRadiationWeight() const;
+    double sunshineHoursWeight() const;
+    double leafWetnessWeight() const;
+    double soilTempWeight() const;
+
+    bool precipitationState() const;
+    bool tAveState() const;
+    bool tMinState() const;
+    bool tMaxState() const;
+    bool relativeHumidityState() const;
+    bool windSpeedState() const;
+    bool globalRadiationState() const;
+    bool sunshineHoursState() const;
+    bool leafWetnessState() const;
+    bool soilTempState() const;
+
+
+    // mutators
+
+    void setNotes (QString theNotes);
+
+    void setPrecipitationMeasured (bool thePrecipitationMeasured);
+    void setTAveMeasured (bool theTAveMeasured);
+    void setTMinMeasured (bool theTMinMeasured);
+    void setTMaxMeasured (bool theTMaxMeasured);
+    void setRelativeHumidityMeasured (bool theRelativeHumidityMeasured);
+    void setWindSpeedMeasured (bool theWindSpeedMeasured);
+    void setGlobalRadiationMeasured (bool theGlobalRadiationMeasured);
+    void setSunshineHoursMeasured (bool theSunshineHoursMeasured);
+    void setLeafWetnessMeasured (bool theLeafWetnessMeasured);
+    void setSoilTempMeasured (bool theSoilTempMeasured);
+
+    void setPrecipitationKmToSite (double thePrecipitationKmToSite);
+    void setTAveKmToSite (double theTAveKmToSite);
+    void setTMinKmToSite (double theTMinKmToSite);
+    void setTMaxKmToSite (double theTMaxKmToSite);
+    void setRelativeHumidityKmToSite (double theRelativeHumidityKmToSite);
+    void setWindSpeedKmToSite (double theWindSpeedKmToSite);
+    void setGlobalRadiationKmToSite (double theGlobalRadiationKmToSite);
+    void setSunshineHoursKmToSite (double theSunshineHoursKmToSite);
+    void setLeafWetnessKmToSite (double theLeafWetnessKmToSite);
+    void setSoilTempKmToSite (double theSoilTempKmToSite);
+
+    void setPrecipitationAltitudeDif (int thePrecipitationAltitudeDif);
+    void setTAveAltitudeDif (int theTAveAltitudeDif);
+    void setTMinAltitudeDif (int theTMinAltitudeDif);
+    void setTMaxAltitudeDif (int theTMaxAltitudeDif);
+    void setRelativeHumidityAltitudeDif (int theRelativeHumidityAltitudeDif);
+    void setWindSpeedAltitudeDif (int theWindSpeedAltitudeDif);
+    void setGlobalRadiationAltitudeDif (int theGlobalRadiationAltitudeDif);
+    void setSunshineHoursAltitudeDif (int theSunshineHoursAltitudeDif);
+    void setLeafWetnessAltitudeDif (int theLeafWetnessAltitudeDif);
+    void setSoilTempAltitudeDif (int theSoilTempAltitudeDif);
+
+    void setPrecipitationOptimumDistance (int thePrecipitationOptimumDistance);
+    void setTAveOptimumDistance (int theTAveOptimumDistance);
+    void setTMinOptimumDistance (int theTMinOptimumDistance);
+    void setTMaxOptimumDistance (int theTMaxOptimumDistance);
+    void setRelativeHumidityOptimumDistance (int theRelativeHumidityOptimumDistance);
+    void setWindSpeedOptimumDistance (int theWindSpeedOptimumDistance);
+    void setGlobalRadiationOptimumDistance (int theGlobalRadiationOptimumDistance);
+    void setSunshineHoursOptimumDistance (int theSunshineHoursOptimumDistance);
+    void setLeafWetnessOptimumDistance (int theLeafWetnessOptimumDistance);
+    void setSoilTempOptimumDistance (int theSoilTempOptimumDistance);
+
+    void setPrecipitationWeight (double thePrecipitationWeight);
+    void setTAveWeight (double theTAveWeight);
+    void setTMinWeight (double theTMinWeight);
+    void setTMaxWeight (double theTMaxWeight);
+    void setRelativeHumidityWeight (double theRelativeHumidityWeight);
+    void setWindSpeedWeight (double theWindSpeedWeight);
+    void setGlobalRadiationWeight (double theGlobalRadiationWeight);
+    void setSunshineHoursWeight (double theSunshineHoursWeight);
+    void setLeafWetnessWeight (double theLeafWetnessWeight);
+    void setSoilTempWeight (double theSoilTempWeight);
+
+    void setPrecipitationState (bool thePrecipitationState);
+    void setTAveState (bool theTAveState);
+    void setTMinState (bool theTMinState);
+    void setTMaxState (bool theTMaxState);
+    void setRelativeHumidityState (bool theRelativeHumidityState);
+    void setWindSpeedState (bool theWindSpeedState);
+    void setGlobalRadiationState (bool theGlobalRadiationState);
+    void setSunshineHoursState (bool theSunshineHoursState);
+    void setLeafWetnessState (bool theLeafWetnessState);
+    void setSoilTempState (bool theSoilTempState);
+
+  private:
+    QString mNotes;
+
+    bool mPrecipitationMeasured;
+    bool mTAveMeasured;
+    bool mTMinMeasured;
+    bool mTMaxMeasured;
+    bool mRelativeHumidityMeasured;
+    bool mWindSpeedMeasured;
+    bool mGlobalRadiationMeasured;
+    bool mSunshineHoursMeasured;
+    bool mLeafWetnessMeasured;
+    bool mSoilTempMeasured;
+
+    double mPrecipitationKmToSite;
+    double mTAveKmToSite;
+    double mTMinKmToSite;
+    double mTMaxKmToSite;
+    double mRelativeHumidityKmToSite;
+    double mWindSpeedKmToSite;
+    double mGlobalRadiationKmToSite;
+    double mSunshineHoursKmToSite;
+    double mLeafWetnessKmToSite;
+    double mSoilTempKmToSite;
+
+    int mPrecipitationAltitudeDif;
+    int mTAveAltitudeDif;
+    int mTMinAltitudeDif;
+    int mTMaxAltitudeDif;
+    int mRelativeHumidityAltitudeDif;
+    int mWindSpeedAltitudeDif;
+    int mGlobalRadiationAltitudeDif;
+    int mSunshineHoursAltitudeDif;
+    int mLeafWetnessAltitudeDif;
+    int mSoilTempAltitudeDif;
+
+    int mPrecipitationOptimumDistance;
+    int mTAveOptimumDistance;
+    int mTMinOptimumDistance;
+    int mTMaxOptimumDistance;
+    int mRelativeHumidityOptimumDistance;
+    int mWindSpeedOptimumDistance;
+    int mGlobalRadiationOptimumDistance;
+    int mSunshineHoursOptimumDistance;
+    int mLeafWetnessOptimumDistance;
+    int mSoilTempOptimumDistance;
+
+    double mPrecipitationWeight;
+    double mTAveWeight;
+    double mTMinWeight;
+    double mTMaxWeight;
+    double mRelativeHumidityWeight;
+    double mWindSpeedWeight;
+    double mGlobalRadiationWeight;
+    double mSunshineHoursWeight;
+    double mLeafWetnessWeight;
+    double mSoilTempWeight;
+
+    bool mPrecipitationState;
+    bool mTAveState;
+    bool mTMinState;
+    bool mTMaxState;
+    bool mRelativeHumidityState;
+    bool mWindSpeedState;
+    bool mGlobalRadiationState;
+    bool mSunshineHoursState;
+    bool mLeafWetnessState;
+    bool mSoilTempState;
+
+
 
 
 };
